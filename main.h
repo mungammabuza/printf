a285c406c6eb7664ca227b238bd2babce2477737
mungammabuza <munga.m.mabuza@gmail.com>
2023-03-26 05:11:52 -0700

    Main.h Update

--- main.h
+++ main.h
@@ -4,5 +4,9 @@
 print_uToBinary(va_list arg);
 int _printf(const char *format, ...);
 void print_buffer(char buffer[], int *buff_ind);
+int print_percent(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_char(va_list types, char buffer[], int flags, int width, int precision, int size);
+int print_string(va_list types, char buffer[], int flags, int width, int precision, int size);
+int  printf_project(va_list types, char buffer[ ], int flags, int width, int precision, int size);
 
 #endif
