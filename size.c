befbd2941b79b178ae69d1da22a7f5a1968b3209
mungammabuza <munga.m.mabuza@gmail.com>
2023-03-27 08:03:45 -0700

    specifiers update

---
+++ size.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * get_size - calculates the size to cast the args
+ * @format: formatted string to print args
+ * @i: list of atgs tonbe printed
+ *
+ * Return: Precisiom
+ */
+int get_size(const char *format, int *i)
+{
+	int curr_i = *i + 1;
+	int size = 0;
+
+	if (format[curr_i] == '1')
+		size = S_LONG;
+	else if (format[curr_i] == 'h')
+		size = S_SHORT;
+
+	if (size == 0)
+		*i = curr_i - 1;
+	else
+		*i = curr_i;
+
+	return (size);
+}
